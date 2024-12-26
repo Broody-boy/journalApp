f8c4d4fc974e46cf9834242fd518a669d379f897
Broody-Boy <arshdeep2875@gmail.com>
2024-12-27 03:02:49 +0530

    Added update userName functionality by taking new username in the body and old one in url Path.

--- src/main/java/net/engineeringdigest/journalApp/controller/UserController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/UserController.java
@@ -29,9 +29,9 @@ public class UserController {
         userService.saveEntry(user);
     }
 
-    @PutMapping
-    public ResponseEntity<?> updateUser(@RequestBody User user) {
-        User userInDb = userService.findByUsername(user.getUserName());
+    @PutMapping("/{userName}")
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String userName) {
+        User userInDb = userService.findByUsername(userName);
         if (userInDb != null) {
             userInDb.setUserName(user.getUserName());
             userInDb.setPassword(user.getPassword());
